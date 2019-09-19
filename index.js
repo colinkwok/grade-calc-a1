6eef9cdab2db4e0d7c5b701b2ae21e924d5a70d9
Colin Kwok <colinkwok@Colins-MacBook-Pro.local>
2019-09-19 10:18:14 -0700

    test

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000
 
-// var app = express();
-express()
-.use(express.static(path.join(__dirname, 'public')))
-.set('views', path.join(__dirname, 'views'))
-.set('view engine', 'ejs')
-.get('/', (req, res) => res.render('pages/calculator.html'))
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+
+// app.get('/', function(request, response){
+//   response.sendFile('calculator.html')
+// });
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
