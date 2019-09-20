4da76cc19623a6f4d0889a568be8c9d5c88ce462
Colin Kwok <colinkwok@Colins-MacBook-Pro.local>
2019-09-19 23:01:54 -0700

    test adding html

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000
-
-
 var app = express();
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
-
-// app.get('/', function(request, response){
-//   response.sendFile('calculator.html')
-// });
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
